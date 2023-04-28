b412df6269e8ffca68c4acfa053cc9709b8129d7
tannu <rawattannu38@gmail.com>
2023-04-29 03:13:26 +0530

    add

--- first_python.py
+++ first_python.py
@@ -1,2 +1,43 @@
-name = input("enter your name")
-print(f"hello,{name}")
+# name = input("enter your name")
+# print(f"hello,{name}") #formated string inside the curly barces we printing the name
+
+# n = int(input("enter the no"))
+
+# if n>0:
+#     print("n is positive")
+
+# elif n<0:
+#     print("n is negative")
+# else:
+#     print("n is non positive")
+
+
+# using set
+#duplicate value not appears in set
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(2)
+print(s)
+s.remove(2)
+print(s)
+
+
+for i in range(8):  #giving the range
+    print(i)
+
+
+# dictionary
+houses = {"harry":"gryfindoor","drago":"slytherin"}
+print(houses["harry"])
+houses["harmaeni"] = "gryfindoor";
+print(houses['harmaeni'])
+
+
+def sum(a,b):
+    print(a+b)
+
+sum(1,2)
+
+
